12a516f024986bab0fa5f6b52877b1b9b645763d
Bolouere1 <86934155+Bolouere1@users.noreply.github.com>
2021-08-22 00:10:28 +0100

    Add files via upload

---
+++ NAME.cpp
@@ -0,0 +1,8 @@
+/*This is a Zuri internship task for stage 2*/
+/*C++ program to print my name*/
+#include <iostream>
+int main()
+{
+    std::cout<< "My name is OSAKWE Bolouere Precious";
+    return 0;
+}
